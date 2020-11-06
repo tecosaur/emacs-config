1f2983be5cbc66d0b661e56be8e35f261e587603
TEC <tec@tecosaur.com>
2020-11-06 18:19:51 +0800

    Org: improve HTML pre-copying

--- misc/pile-css-theme/_code.js
+++ misc/pile-css-theme/_code.js
@@ -1,7 +1,8 @@
 function copyPreToClipdord(btn) {
     const pre = btn.parentElement.parentElement.getElementsByTagName("PRE")[0];
     const range = document.createRange();
-    range.selectNode(pre);
+    range.selectNodeContents(pre);
+    range.setEnd(pre.childNodes[pre.childNodes.length-1], 0);
     window.getSelection().addRange(range);
     var successful = document.execCommand('copy');
     window.getSelection().removeRange(range);
