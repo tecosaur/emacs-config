1962cb211be869e723af6aac286abc0e90e61325
TEC <tec@tecosaur.com>
2021-04-17 12:33:46 +0800

    Org: HTML, add Expat licence magnet to included JS

---
+++ misc/org-css/_~magnet_licence.js
@@ -0,0 +1,1 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
--- misc/org-css/main.js
+++ misc/org-css/main.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
 function copyPreToClipbord(btn) {
     const pre = btn.parentElement.parentElement.getElementsByTagName("PRE")[0];
     const range = document.createRange();
@@ -38,3 +39,4 @@ window.addEventListener('DOMContentLoaded', () => {
         observer.observe(section);
     });
 });
+// @license-end
